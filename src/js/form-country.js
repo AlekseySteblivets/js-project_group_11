447cb8b4430ebeaefd8d24ca0c5d0137dac70a19
ROMANTFORM <62114302+ROMANTFORM@users.noreply.github.com>
2021-05-10 23:13:53 +0300

    выпадалка

--- src/js/form-country.js
+++ src/js/form-country.js
@@ -1,1 +1,8 @@
 
+import { refs } from './objects-refs';
+
+refs.formChooseEl.addEventListener('focus', onCountryChoose);
+
+function onCountryChoose() {
+    refs.countryInputEl.classList.add('input-country--active');
+};
--- src/js/index.js
+++
@@ -1,2 +0,0 @@
-
-import './form-country.js';
