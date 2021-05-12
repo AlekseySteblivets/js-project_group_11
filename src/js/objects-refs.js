6b9abbb1c25e525d02d534c0aec15098167640f3
ROMANTFORM <62114302+ROMANTFORM@users.noreply.github.com>
2021-05-12 20:48:53 +0300

    close modal

--- src/js/objects-refs.js
+++ src/js/objects-refs.js
@@ -1,7 +1,7 @@
 export const refs = {
     formEl: document.querySelector('.js-form'),
-    formSearchEl: document.querySelector('.js-input__search'),
-    formChooseEl: document.querySelector('.js-input__choose'),
+    formSearchEl: document.querySelector('.js-form__search'),
+    formChooseEl: document.querySelector('.js-form__choose'),
     cardListEl: document.querySelector('.main-cards'),
     modalEl: document.querySelector('.modal'),
     modalCloseBtn: document.querySelector('.modal-close__btn'),
