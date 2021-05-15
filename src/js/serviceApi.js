72f5e65e30985f5c2d303f73747dd8a37b4fbe10
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-05-15 17:57:14 +0300

    Удаляет кетч Ромы

--- src/js/serviceApi.js
+++ src/js/serviceApi.js
@@ -4,11 +4,6 @@ import tamplateCountryName from '../templates/countryName.hbs';
 import arrCountries from './countries-name';
 
 // ========== PNotify ============================================================
-import '@pnotify/core/dist/BrightTheme.css';
-import { alert, notice, info, success, error } from '@pnotify/core';
-import { defaults } from '@pnotify/core';
-
-defaults.addClass = 'my-pnotify';
 
 const KEY = '0PSOw59QQHJn14wudWQZ3vLoS3PmgpC6';
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
@@ -54,11 +49,8 @@ function searchCountryOfName(countryCode) {
             
         })
         .catch(error => {
-            notice({
-                text: "В этой стране нет мероприятий! Выберите другую страну!",
-                style: 'top: 0',
-                });
-            console.log(error)
+
+          console.log(error)
         })
 };
 // ====================================================================================
