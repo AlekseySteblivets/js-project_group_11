5ba1eb3092921ffe5636e9aa86d53673d601acbe
ROMANTFORM <62114302+ROMANTFORM@users.noreply.github.com>
2021-05-17 13:57:22 +0300

    correct modal

--- src/js/modalLogic.js
+++ src/js/modalLogic.js
@@ -1,24 +1,31 @@
 import eventCard from '../templates/eventCard.hbs';
 import { refs } from './objects-refs';
-import { fetchData } from './serviceApi';
+
+
+const KEY = '0PSOw59QQHJn14wudWQZ3vLoS3PmgpC6';
+const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
 
 refs.cardListEl.addEventListener('click', onEventsClick);
 
 function onEventsClick(e) {
   const eventId = e.target.id;
+  console.log(eventId);
+  fetch(`${BASE_URL}/events/${eventId}.json?&apikey=${KEY}`)
+    .then(res => res.json())
+    .then(data => {
+      markup(data);
+      const infoContainer = document.querySelector('.info-container__img');
+      const postContainer = document.querySelector('.info-wrapper__poster');
+      infoContainer.innerHTML = `<img src=${data.images[6].url} class="poster-img">`;
+      postContainer.innerHTML = `<img src=${data.images[2].url}>`
+    })
+    .catch(error => console.log(error));
+  
   refs.modalWindow.classList.add('is-open');
-
-  fetchData.then(data => {
-    const events = data._embedded.events;
-    const result = events.find(({ id }) => id === eventId);
-    markup(result);
-  });
-}
+};
 
 function markup(data) {
   refs.modalContainer.innerHTML = eventCard(data);
-}
+};
 
-function errror() {
-  alert('error');
-}
+
