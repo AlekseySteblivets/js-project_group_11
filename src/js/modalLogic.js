92c6dc8985eadf576c7281d5ad84f7219ac24fa2
Alexander Khomenko <72103285+xomuss@users.noreply.github.com>
2021-05-18 15:13:37 +0300

    modalFix

--- src/js/modalLogic.js
+++ src/js/modalLogic.js
@@ -1,6 +1,6 @@
 import eventCard from '../templates/eventCard.hbs';
 import { refs } from './objects-refs';
-
+import {fetchData} from './serviceApi'
 
 const KEY = '0PSOw59QQHJn14wudWQZ3vLoS3PmgpC6';
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
@@ -10,16 +10,21 @@ refs.cardListEl.addEventListener('click', onEventsClick);
 function onEventsClick(e) {
   const eventId = e.target.id;
   console.log(eventId);
-  fetch(`${BASE_URL}/events/${eventId}.json?&apikey=${KEY}`)
-    .then(res => res.json())
-    .then(data => {
-      markup(data);
-      const infoContainer = document.querySelector('.info-container__img');
-      const postContainer = document.querySelector('.info-wrapper__poster');
-      infoContainer.innerHTML = `<img src=${data.images[6].url} class="poster-img">`;
-      postContainer.innerHTML = `<img src=${data.images[2].url}>`
-    })
-    .catch(error => console.log(error));
+  console.log(fetchData);
+  const currentEvent = fetchData.find(item => item.id === eventId);
+  console.log(currentEvent);
+  markup(currentEvent)
+  // console.log(eventId);
+  // fetch(`${BASE_URL}/events/${eventId}.json?&apikey=${KEY}`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     markup(data);
+  //     const infoContainer = document.querySelector('.info-container__img');
+  //     const postContainer = document.querySelector('.info-wrapper__poster');
+  //     infoContainer.innerHTML = `<img src=${data.images[6].url} class="poster-img">`;
+  //     postContainer.innerHTML = `<img src=${data.images[2].url}>`
+  //   })
+  //   .catch(error => console.log(error));
   
   refs.modalWindow.classList.add('is-open');
 };
