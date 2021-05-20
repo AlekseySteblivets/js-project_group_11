71cba38a2f90c7d29fa556efa1dc2177773867d1
KristinaY-87 <72267606+KristinaY-87@users.noreply.github.com>
2021-05-20 23:21:57 +0300

    fixed style

--- src/js/serviceApi.js
+++ src/js/serviceApi.js
@@ -173,18 +173,18 @@ const options = {
   // firstItemClassName: 'tui-first-child',
   // lastItemClassName: 'tui-last-child',
   template: {
-    page: '<a href="#" class=" pagination-link">{{page}}</a>',
-    currentPage: '<strong class=" pagination-link active">{{page}}</strong>',
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
     moveButton:
-      '<a href="#" class=" pagination-link ">' +
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</a>',
     disabledMoveButton:
-      '<span class="pagination-link ">' +
-      '<span class="pagination-link"></span>' +
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</span>',
     moreButton:
-      '<a href="#" class="pagination-link">' +
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
       '<span class="tui-ico-ellip">...</span>' +
       '</a>'
   }
